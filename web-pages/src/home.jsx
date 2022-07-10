ef34535beca6729d56b6046e68d0118a8b8bd083
ishaans823 <73730967+ishaans823@users.noreply.github.com>
2022-07-10 00:12:42 -0400

    Add files via upload

---
+++ web-pages/src/home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+<<<<<<< HEAD
+import background from "./images/background.jpeg"
+=======
+>>>>>>> c35d5fe7178dc55a21d19d5f78dede0c5ae25881
+
+
+const MainPage = () => {
+    return (
+<<<<<<< HEAD
+        <div style={{ backgroundImage:`url(${background})`,backgroundRepeat:"no-repeat",backgroundSize:"contain", 
+    height:600,width:1000
+    }}>    </div>
+=======
+        <h1> Hello World </h1>
+>>>>>>> c35d5fe7178dc55a21d19d5f78dede0c5ae25881
+    );
+};
+
+export default MainPage;
