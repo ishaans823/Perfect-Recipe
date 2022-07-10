a70e9c606d781b661c2a7300da9338f41ebcebb1
Ishaan Shah <ishaan.shah823@gmail.com>
2022-07-09 23:55:35 -0400

    Added login page, needs functionality

--- web-pages/src/App.js
+++ web-pages/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link, Redirect} from 'react-router-dom'
 import MainPage from './pages /home';
 import ErrorPage from './pages /error';
+import LoginPage from './pages /login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<MainPage/>} />
         <Route path='*' element={<ErrorPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
       </Routes>
     </Router>
   );
