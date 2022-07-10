7003a59585bb1dca5cecd99ec42c25eed6a86767
ishaans823 <73730967+ishaans823@users.noreply.github.com>
2022-07-10 00:12:42 -0400

    Add files via upload

---
+++ web-pages/src/error.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const ErrorPage = () => {
+    return (
+        <h1> Oops, Invalid Page! </h1>
+    );   
+};
+
+<<<<<<< HEAD
+export default ErrorPage;
+=======
+export default ErrorPage;
+>>>>>>> c35d5fe7178dc55a21d19d5f78dede0c5ae25881
